243c2576d8a5e1460fffd09fd3fe49c46070c0c2
Alexander Pino <alexanderpino@hotmail.com>
2022-06-12 22:42:49 -0500

    Creacion de las clases del proyecto uber, en Js, Java, PHP y Python

---
+++ PHP/Accont.php
@@ -0,0 +1,9 @@
+class Account{
+    <?php
+    public $id = integer;
+    public $name = string;
+    public $document = string;
+    public $email = string;
+    public $password = string;
+
+}
---
+++ PHP/Car.php
@@ -0,0 +1,8 @@
+<?php
+class car {
+
+    public $id = integer;
+    public $license = string;
+    public $driver = string;
+    public $passengers = integer;
+}
