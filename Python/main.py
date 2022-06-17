0795b89244377273676a097ac715e9591d564f5b
Alexander Pino <alexanderpino@hotmail.com>
2022-06-16 22:28:13 -0500

    desafio cuatro, falta codigo en main.py

--- Python/main.py
+++ Python/main.py
@@ -1,9 +1,10 @@
 from car import Car
 from account import Account
+from uberX import UberX
+from user import user
 
 if __name__ == "__main__":
-    print("Hola mundo")
-    
+    print("Iniciando informacion de la base Uber")    
     car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
     print(vars(car))
     print(vars(car.driver))
