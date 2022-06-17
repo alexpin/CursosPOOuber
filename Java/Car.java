be7e5795aa6698a501aab685ce063d2e231d39fb
Alexander Pino <alexanderpino@hotmail.com>
2022-06-16 23:30:06 -0500

    Encapsulamiento uno, Java

--- Java/Car.java
+++ Java/Car.java
@@ -3,17 +3,19 @@ public class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passegenger;
+    private Integer passegenger;
 
     public Car(String license, Account driver){ // Metodo constructor
 
        this.license = license;
        this.driver = driver; 
-    }
+       passegenger = 3;
+
+       }
 
     void prinDataCar(){
-
-        System.out.println("License: " + license + " Name Driver: " + driver.name);
+        System.out.println("Passengers: " + passegenger);
+        System.out.println("License: " + license + " Name Driver: " + driver.name + " Passengers: " + passegenger);
         
     }
     
--- Java/Main.java
+++ Java/Main.java
@@ -1,14 +1,14 @@
 class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo");
+        System.out.println("Iniciando base de datos Uber.");
 
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123")); // Crea objeto car con su metodo constructor
-        car.passegenger = 4;  
-        car.prinDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic"); // Crea objeto car con su metodo constructor
+        //uberX.passegenger = 4;  
+        uberX.prinDataCar();
 
-        Car car2 = new Car("QME567", new Account("Andrea Herrera", "ANDA876"));
+        /*Car car2 = new Car("QME567", new Account("Andrea Herrera", "ANDA876"));
         car2.passegenger = 3;
-        car2.prinDataCar();        
+        car2.prinDataCar();  */      
     }
 }
